e2d53496b2dc08be1b05671d56f9d9ee617a8ef7
MaazGhazi <maaz.ghazi1738@gmail.com>
2026-01-15 19:03:13 -0500

    added googleanalytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { fredoka, rubik } from "@/lib/fonts";
 import "./globals.css";
 
@@ -15,6 +16,31 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${fredoka.variable} ${rubik.variable}`}>
       <body className="antialiased">
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WS29K6XJ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WS29K6XJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
       </body>
     </html>
